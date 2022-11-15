bf15d724220b6b3dd638e7cbc912e7f0644f738d
Namig N. Abilov <namigna@code.local>
2022-11-15 10:15:06 +0400

    commit

--- JS Task-2/Js/app.js
+++ JS Task-2/Js/app.js
@@ -16,32 +16,22 @@ else{
 
 
 
-//  function check() {
-//      var startNum = 1 ;
-//     var inputValue = document.getElementById('group').value;
-//     for (var i = 0; i < inputValue.length; i++) {
-//         if (inputValue[i] !== NaN) {
-//              startNum=i;
-//              break;
-//         }
-//      }
-    
-//     document.getElementById('text').innerHTML = startNum;
-// }
-//  Mellim burda men for ile ilk reqemin indexsin goturub slice ile kesmek isteyirdim ama burdaki break islemedi  
-
-function check() {
-     var inputValue = document.getElementById('group').value;
-     var number = inputValue.slice(-3);
-     var smen = "";
-    number = number.slice(0,1);
-    if (number == 1) {
+  function check() {
+     var startNum = 1 ;
+    var inputValue = document.getElementById('group').value;
+    for (var i = 0; i < inputValue.length; i++) {
+        if (!isNaN(inputValue[i])) {
+             startNum=inputValue[i]; 
+             break;
+        }
+     }
+     if (startNum == 1) {
         smen += " Seher ";
     }
-    else if(number == 2){
+    else if(startNum == 2){
         smen += " Gunorta ";
     }
-    else if(number == 3){
+    else if(startNum == 3){
         smen += " Axsam ";
     }
     else{
@@ -50,3 +40,25 @@ function check() {
     var text = " Sizin Qrupunuz " + inputValue +  smen + " Smenidir !"  
     document.getElementById('text').innerHTML = text;
 }
+//  Mellim burda men for ile ilk reqemin indexsin goturub slice ile kesmek isteyirdim ama burdaki break islemedi  
+
+// function check() {
+//      var inputValue = document.getElementById('group').value;
+//      var number = inputValue.slice(-3);
+//      var smen = "";
+//     number = number.slice(0,1);
+//     if (number == 1) {
+//         smen += " Seher ";
+//     }
+//     else if(number == 2){
+//         smen += " Gunorta ";
+//     }
+//     else if(number == 3){
+//         smen += " Axsam ";
+//     }
+//     else{
+//         smen+= " Bilinmeyen  "
+//     }
+//     var text = " Sizin Qrupunuz " + inputValue +  smen + " Smenidir !"  
+//     document.getElementById('text').innerHTML = text;
+// }
