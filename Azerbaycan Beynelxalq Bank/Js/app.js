2dece2e108777fe8db0c0db55a550826a2f15379
namigabilov <111847013+namigabilov@users.noreply.github.com>
2022-11-22 18:56:40 +0400

    .

--- Azerbaycan Beynelxalq Bank/Js/app.js
+++
@@ -1,7 +0,0 @@
-var layer = document.getElementById('layer');
-var search = document.getElementById('search'); 
-search.onclick = function(){
-    layer.classList.toggle('active');
-    var searchMain = document.getElementById('searchMain'); 
-    searchMain.classList.toggle('active2');
-}
